3a81ba9a82e835ddd42e36c35914544c821f0806
clementfourmis <91278704+clementfourmis@users.noreply.github.com>
2022-01-30 20:33:42 +0100

    la troisième c'est la bonne ?
    correction

--- json/supermarches.js
+++ json/supermarches.js
@@ -1237,6 +1237,7 @@ export const supermarches = [
     lattitude: 38.713767,
     longitude: -9.136232
   },
+
   {
     nom: 'Coop des Luys',
     departement: '64',
@@ -1249,6 +1250,7 @@ export const supermarches = [
     lattitude: 43.404078,
     longitude: -0.38633
   },
+
   {
     nom: 'Graoucoop',
     departement: '57',
